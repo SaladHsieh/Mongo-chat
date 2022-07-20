de1de57914669764c8c41f6ca3a57316c4a017fa
Sarah <hsue1207@gmail.com>
2022-07-20 11:08:59 +0800

    feat: mongoDB connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,17 +9,54 @@ import {
   userLeave,
   getRoomUsers,
 } from './utils/users';
-// import './utils/mongoose';
+import './utils/mongoose';
+import mongoose from 'mongoose';
+import { createBuilderStatusReporter } from 'typescript';
 require('dotenv').config();
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+// mongoDB connection START
+const DB = process.env.DATABASE!.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD!
+);
+
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user must have a name!'],
+    unique: true,
+  },
+  gender: {
+    type: String,
+  },
+  description: {
+    type: String,
+    required: false,
+    default: 'hi everyone',
+  },
+});
+
+const Users = mongoose.model('Users', userSchema);
+
+const testUser = new Users({
+  name: 'John',
+  gender: 'male',
+  description: 'Have a good day!',
+});
+
+testUser
+  .save()
+  .then((doc: any) => console.log(doc))
+  .catch((err: any) => console.log(err));
+
 // Set static folder
 app.use(express.static(path.join(__dirname, '/../public')));
-
-// const botName = 'Chatroom';
 
 let getRoomUsersFn: any;
 
@@ -27,8 +64,9 @@ let getRoomUsersFn: any;
 io.on('connection', (socket: any) => {
   socket.on(
     'joinRoom',
-    ({ username, room }: { username: string; room: string }) => {
-      const user = userJoin(socket.id, username, room);
+    ({ username, room }: { username: string; room: string }, cb: any): any => {
+      const { err, user } = userJoin(socket.id, username, room);
+      if (err) return cb(err);
 
       socket.join(user.room);
 
@@ -54,6 +92,7 @@ io.on('connection', (socket: any) => {
         });
       };
       getRoomUsersFn();
+      cb();
     }
   );
   // Listen for chatMessage from front-end
